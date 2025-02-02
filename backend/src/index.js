11ae89084cbbb4f69e9a9f454cdec21b9075f0f6
Akshay-bendadi <akshaybendadi@gmail.com>
2025-02-02 13:43:18 +0530

    error solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  // Serve static files from the correct dist folder
+  app.use(express.static(path.join(__dirname, "../../frontend/src/dist")));
 
-
+  // Serve index.html for all other routes
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/src/dist/index.html"));
   });
 }
 
