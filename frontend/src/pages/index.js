52d5d93ac7c9bc4af86610c8dd12825f9eb339b3
Akshay-bendadi <akshaybendadi@gmail.com>
2025-02-02 12:58:32 +0530

    Before deployment

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import SettingPage from "./SettingPage";
+import SignUpPage from "./SignUpPage";
+
+export { HomePage, LoginPage, ProfilePage, SettingPage, SignUpPage };
